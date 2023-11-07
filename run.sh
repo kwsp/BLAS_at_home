20282de26aacc1d24097274df6d13d3e01938fa9
tnie <tnie@tuta.io>
2023-11-07 17:01:51 -0600

    comment run script

--- run.sh
+++ run.sh
@@ -1,13 +1,16 @@
+#!/bin/bash
 set -e
 
+# Build with CMake
 cmake --preset=clang-with-warnings -B build
 cmake --build build -j
 
+# Link compile_commands.json
 if [[ -f compile_commands.json ]]; then
   rm compile_commands.json
 fi
-
 ln -s ./build/compile_commands.json compile_commands.json
 
+# Run gtest
 echo
 ./build/bah_test
